debaefd12aeedddffd64005e27ff368047e85442
e-goncalves <e-goncalves@c5718f34-3edb-49f3-92da-48174758b353>
2012-11-26 10:38:13 +0000

    Plug-in version increased to v1.2

--- cysbgn/compile.sh
+++ cysbgn/compile.sh
@@ -6,5 +6,5 @@ ant jar
 
 echo "[MOVE JAR]"
 echo ""
-chmod +x CySBGN-v1.1.jar
-mv CySBGN-v1.1.jar /Applications/Cytoscape_v2.8.3/plugins/
+chmod +x CySBGN-v1.2.jar
+mv CySBGN-v1.2.jar /Applications/Cytoscape_v2.8.3/plugins/
